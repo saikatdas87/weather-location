9a276e2fa6fcd9061cde4efb64e04706ddd86856
saikat-das87 <saikat.das@cloudator.com>
2020-08-06 12:51:57 +0300

    create structure

---
+++ src/main/java/com/saikat/project/weatherlocation/WeatherLocationApplication.java
@@ -0,0 +1,13 @@
+package com.saikat.project.weatherlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saikat/project/weatherlocation/WeatherLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saikat.project.weatherlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
